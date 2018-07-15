fcbe97e734be7b84de4ca68dcdac46bed6a95110
Mahipal Reddy <mahireddy45@gmail.com>
2018-07-15 12:58:54 +0530

    Updated 3 approaches

--- Interest-Group/src/com/test/arrays/ArrayRotate.java
+++ Interest-Group/src/com/test/arrays/ArrayRotate.java
@@ -2,9 +2,10 @@ package com.test.arrays;
 
 public class ArrayRotate {
 	
-	//Using auxiliray array for rotation
-	private static void auxArrRotation(int[] arr, int d) {
-		int len = arr.length;
+	//Using auxiliary array of size d for rotation
+	//Space Complexity O(d)
+	//Time Complexity O(n)
+	private static void auxArrRotation(int[] arr, int d, int len) {
 		int[] auxArr = new int[d];
 		for(int i =0; i<d; i++) {
 			auxArr[i] = arr[i];
@@ -17,13 +18,52 @@ public class ArrayRotate {
 		}
 	}
 	
+	//Approach 2: 
+	//Rotate one by one 
+	//Time Complexity: O(d*n)
+	private static void rotateOneByOne(int[] arr, int d, int len) {
+		for(int i=0; i<d; i++) {
+			int temp = arr[0];
+			int j;
+			for(j=0; j<len-1; j++) {
+				arr[j] = arr[j+1];
+			}
+			arr[j] = temp;
+		}
+	}
+	
+	//Approach 3: 
+	//Reverse starting to d 
+	//Reverse d to len-1
+	//Reverse 0 to len-1
+	//Time Complexity: O(n)
+	private static void reverseLogicUsingArrRotation(int[] arr, int d, int len) {
+		reverse(arr, 0, d-1);
+		reverse(arr, d, len-1);
+		reverse(arr, 0, len-1);
+	}
+	private static void reverse(int[] arr, int i, int d) {
+		while(i<d) {
+			int temp = arr[i];
+			arr[i] = arr[d];
+			arr[d] = temp;
+			i++; d--;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// Given size (d) rotation of array arr
 		int[] arr = {1,2,3,4,5,6,7,8};
 		int d = 3; 
+		int len = arr.length;
+		//Apporach1 
+		//auxArrRotation(arr, d, len);	
 		
-		//Call Apporach1 
-		auxArrRotation(arr, d);	
+		//Apporach2
+		//rotateOneByOne(arr, d, len);
+		
+		//Approach3
+		reverseLogicUsingArrRotation(arr, d, len);
 		
 		for(int i=0; i<arr.length; i++)
 			System.out.println(arr[i]);

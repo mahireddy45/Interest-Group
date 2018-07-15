66d24319f5ddd3855fadab3f83bdcc135c6c8ff2
Mahipal Reddy <mahireddy45@gmail.com>
2018-07-15 11:36:52 +0530

    Array rotation program first approach

--- Interest-Group/src/com/test/arrays/ArrayRotate.java
+++ Interest-Group/src/com/test/arrays/ArrayRotate.java
@@ -1,10 +1,31 @@
 package com.test.arrays;
 
 public class ArrayRotate {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//Using auxiliray array for rotation
+	private static void auxArrRotation(int[] arr, int d) {
+		int len = arr.length;
+		int[] auxArr = new int[d];
+		for(int i =0; i<d; i++) {
+			auxArr[i] = arr[i];
+		}
+		for(int i=d; i<len; i++) {
+			arr[i-d] = arr[i];
+		}
+		for(int i=0; i<d; i++) {
+			arr[len-d+i] = auxArr[i];
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		// Given size (d) rotation of array arr
+		int[] arr = {1,2,3,4,5,6,7,8};
+		int d = 3; 
+		
+		//Call Apporach1 
+		auxArrRotation(arr, d);	
+		
+		for(int i=0; i<arr.length; i++)
+			System.out.println(arr[i]);
+	}
 }
